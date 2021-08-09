9e0182d8871653b92baa3cad79fb9b9f346fda26
coryfree <48961288+coryfree@users.noreply.github.com>
2021-08-09 14:24:20 -0500

    Updated tables.sql
    Added the devices and dataplans tables

--- tables.sql
+++ tables.sql
@@ -6,3 +6,20 @@ create table user(
     password varchar(50) not null,
     primary key (userID)
 );
+
+CREATE TABLE devices (
+  userID INT NOT NULL,
+  deviceID INT NOT NULL AUTO_INCREMENT, 
+  deviceName VARCHAR(45) NOT NULL,
+  phoneNumber VARCHAR(45) NOT NULL,
+  deviceDescription VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`deviceID`)
+);
+  
+CREATE TABLE dataplans (
+  userID INT NULL,
+  dataPlanID INT NOT NULL AUTO_INCREMENT,
+  numberOfDevices INT NOT NULL DEFAULT 0,
+  planDescription VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`dataPlanID`)
+);
