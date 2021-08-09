5f91450da716aad7bf800a66881f2adf52e2524d
Chris4545 <C.ung3232@gmail.com>
2021-08-09 14:25:54 -0500

    changed varchar limit to be consistent

--- tables.sql
+++ tables.sql
@@ -1,9 +1,9 @@
 create table user(
     userID int not null auto_increment,
-    firstName varchar(50) not null,
-    lastName varchar(50) not null,
-    email varchar(50) not null,
-    password varchar(50) not null,
+    firstName varchar(45) not null,
+    lastName varchar(45) not null,
+    email varchar(45) not null,
+    password varchar(45) not null,
     primary key (userID)
 );
 
