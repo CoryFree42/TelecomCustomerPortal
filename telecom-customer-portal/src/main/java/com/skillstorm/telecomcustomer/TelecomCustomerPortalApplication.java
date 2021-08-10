02746b3bb30ceaef3429f78c3f3bd5b388fa1419
Chris4545 <C.ung3232@gmail.com>
2021-08-10 07:43:07 -0500

    Added spring starter initial packages

---
+++ telecom-customer-portal/src/main/java/com/skillstorm/telecomcustomer/TelecomCustomerPortalApplication.java
@@ -0,0 +1,13 @@
+package com.skillstorm.telecomcustomer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelecomCustomerPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelecomCustomerPortalApplication.class, args);
+	}
+
+}
---
+++ telecom-customer-portal/src/test/java/com/skillstorm/telecomcustomer/TelecomCustomerPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skillstorm.telecomcustomer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelecomCustomerPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
