0706363b89d97e629ed5cda337e18715588f9144
Chris4545 <C.ung3232@gmail.com>
2021-08-11 07:32:05 -0500

    made changes to table sql

--- tables.sql
+++ tables.sql
@@ -1,22 +1,27 @@
 CREATE TABLE users(
     userID INT NOT NULL AUTO_INCREMENT,
+    dataPlanID INT NOT NULL,
     firstName VARCHAR(45) NOT NULL,
     lastName VARCHAR(45) NOT NULL,
     email VARCHAR(45) NOT NULL,
     password VARCHAR(45) NOT NULL,
-    PRIMARY KEY (`userID`)
+    PRIMARY KEY (`userID`),
+    FOREIGN KEY (`dataPlanID`)
+    REFERENCES data_plans(dataPlanID)
 );
 
 CREATE TABLE devices (
+  deviceID INT NOT NULL AUTO_INCREMENT,
   userID INT NOT NULL,
-  deviceID INT NOT NULL AUTO_INCREMENT, 
   deviceName VARCHAR(45) NOT NULL,
   phoneNumber VARCHAR(45) NOT NULL,
   deviceDescription VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`deviceID`)
+  PRIMARY KEY (`deviceID`),
+  FOREIGN KEY (`userID`)
+  REFERENCES users(userID)
 );
   
-CREATE TABLE dataplans (
+CREATE TABLE data_plans (
   userID INT NULL,
   dataPlanID INT NOT NULL AUTO_INCREMENT,
   numberOfDevices INT NOT NULL DEFAULT 0,
