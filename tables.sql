46e8a31caba0e251f2536b0120d75e0e2ab37f97
Chris4545 <C.ung3232@gmail.com>
2021-08-11 09:41:09 -0500

    Finalized SQL create statements

--- tables.sql
+++ tables.sql
@@ -1,30 +1,29 @@
 CREATE TABLE users(
     userID INT NOT NULL AUTO_INCREMENT,
-    dataPlanID INT NOT NULL,
     firstName VARCHAR(45) NOT NULL,
     lastName VARCHAR(45) NOT NULL,
     email VARCHAR(45) NOT NULL,
     password VARCHAR(45) NOT NULL,
-    PRIMARY KEY (`userID`),
-    FOREIGN KEY (`dataPlanID`)
-    REFERENCES data_plans(dataPlanID)
+    PRIMARY KEY (`userID`)
 );
 
 CREATE TABLE devices (
-  deviceID INT NOT NULL AUTO_INCREMENT,
-  userID INT NOT NULL,
-  deviceName VARCHAR(45) NOT NULL,
   phoneNumber VARCHAR(45) NOT NULL,
+  userID INT NOT NULL,
+  dataPlanID INT NOT NULL,
+  deviceName VARCHAR(45) NOT NULL,
   deviceDescription VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`deviceID`),
+  PRIMARY KEY (`phoneNumber`),
   FOREIGN KEY (`userID`)
-  REFERENCES users(userID)
+  REFERENCES users(userID),
+  FOREIGN KEY (`dataPlanID`)
+  references data_plans(dataPlanID)
 );
   
 CREATE TABLE data_plans (
-  userID INT NULL,
   dataPlanID INT NOT NULL AUTO_INCREMENT,
   numberOfDevices INT NOT NULL DEFAULT 0,
   planDescription VARCHAR(45) NOT NULL,
+  costPerDevice DOUBLE NOT NULL,
   PRIMARY KEY (`dataPlanID`)
 );
