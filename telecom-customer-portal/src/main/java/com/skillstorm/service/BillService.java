06d42dc1d6e76eaac46829d1f56d5cd2c4276823
Chris4545 <C.ung3232@gmail.com>
2021-08-13 09:22:43 -0500

    Fixed issue with null errors in bill service and device repository

--- telecom-customer-portal/src/main/java/com/skillstorm/service/BillService.java
+++ telecom-customer-portal/src/main/java/com/skillstorm/service/BillService.java
@@ -14,17 +14,18 @@ public class BillService {
 public double getBill(User user) {
 		
 		Double total = 0.0;
-		total += repository.getBill(user.getUserID(), 1);
-		System.out.println(total);
-		total += repository.getBill(user.getUserID(), 2);
-		System.out.println(total);
-		total += repository.getBill(user.getUserID(), 3);
-		System.out.println(total);
-
+		Double plan1 = repository.getBill(user.getUserID(), 1);
+		Double plan2 = repository.getBill(user.getUserID(), 2);
+		Double plan3 = repository.getBill(user.getUserID(), 3);
+        if(plan1 != null) total+= plan1;
+        if(plan2 != null) total+= plan2;
+        if(plan3 != null) total+= plan3;
+		
 		return total;
 	}
 	
 	public double getBill(int planID) {
 		return 0;
 	}
+	
 }
