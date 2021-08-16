b684949b404f5a2b7f2574430f5239ebecbf3215
Chris4545 <C.ung3232@gmail.com>
2021-08-16 10:25:18 -0500

    Basic bill page functionality, will need to modify how user gets chosen later and add stylizations to make it look nice

--- telecom-portal-angular/src/app/bill/bill.component.ts
+++ telecom-portal-angular/src/app/bill/bill.component.ts
@@ -11,6 +11,9 @@ import User from '../models/User';
 export class BillComponent implements OnInit {
 
   bill:number;
+  plan1:number;
+  plan2:number;
+  plan3:number;
   user:User;
   billService:BillService;
   userService:UserService;
@@ -19,6 +22,9 @@ export class BillComponent implements OnInit {
     this.billService = bservice;
     this.userService = uservice;
     this.bill = 0;
+    this.plan1 = 0;
+    this.plan2 = 0;
+    this.plan3 = 0;
     this.user = {userID:0, firstName:"daw", lastName:"wadadw", email:"Chung@gmail.com", password:"adwda"};
 
   }
@@ -26,10 +32,23 @@ export class BillComponent implements OnInit {
   ngOnInit(): void {
     this.userService.getAllUsers().subscribe(result => {
       this.user = result[0];
+      console.log(this.user);
+
+      this.billService.getBill(this.user).subscribe(result => {
+        this.bill = result;
+        console.log(result);
+      })
+      this.billService.getABill(this.user, 1).subscribe(result => {
+        this.plan1 = result;
+      })
+      this.billService.getABill(this.user, 2).subscribe(result => {
+        this.plan2 = result;
+      })
+      this.billService.getABill(this.user, 3).subscribe(result => {
+        this.plan3 = result;
+      })
    })
-   this.billService.getBill(this.user).subscribe(result => {
-     this.bill = result;
-   })
+   
   }
 
 }
