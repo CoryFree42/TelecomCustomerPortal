54de8b7e7c59f27817a4a8d4da45de24ff2103d2
coryfree <coryfree@att.net>
2021-08-16 07:49:11 -0500

    Fixed the CORS policy issue when making requests through the user controller class.

--- telecom-customer-portal/src/main/java/com/skillstorm/controllers/UserController.java
+++ telecom-customer-portal/src/main/java/com/skillstorm/controllers/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,6 +27,7 @@ import com.skillstorm.service.UserService;
 
 import net.bytebuddy.dynamic.DynamicType.Builder.FieldDefinition.Optional;
 
+@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 @RestController
 @RequestMapping("/users")
 public class UserController {
