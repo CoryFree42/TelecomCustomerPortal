68bde6b02b35f325aae0ab5a8a734a14ba712261
Chris4545 <C.ung3232@gmail.com>
2021-08-16 09:48:21 -0500

    WIP bill service and bill component stuff

--- telecom-portal-angular/src/app/bill/bill.component.ts
+++ telecom-portal-angular/src/app/bill/bill.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { BillService } from '../bill.service';
+import { UserService } from '../user.service';
+import User from '../models/User';
 
 @Component({
   selector: 'app-bill',
@@ -7,9 +10,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BillComponent implements OnInit {
 
-  constructor() { }
+  bill:number;
+  user:User;
+  billService:BillService;
+  userService:UserService;
+
+  constructor(bservice: BillService, uservice:UserService) { 
+    this.billService = bservice;
+    this.userService = uservice;
+    this.bill = 0;
+    this.user = {userID:0, firstName:"daw", lastName:"wadadw", email:"Chung@gmail.com", password:"adwda"};
+
+  }
 
   ngOnInit(): void {
+    this.userService.getAllUsers().subscribe(result => {
+      this.user = result[0];
+   })
+   this.billService.getBill(this.user).subscribe(result => {
+     this.bill = result;
+   })
   }
 
 }
