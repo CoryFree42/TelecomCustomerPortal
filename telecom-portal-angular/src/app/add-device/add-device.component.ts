5f88651bfbdbcd8f6ad2fc9f47c627f65a2c57f8
Chris4545 <C.ung3232@gmail.com>
2021-08-16 16:21:23 -0500

    Partially finished bill form and added a userManager service(may be deleted later, in testing)

--- telecom-portal-angular/src/app/add-device/add-device.component.ts
+++ telecom-portal-angular/src/app/add-device/add-device.component.ts
@@ -1,20 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { DeviceService } from '../device.service';
+import { observable } from 'rxjs';
 import Device from '../models/Device';
 @Component({
   selector: 'app-add-device',
   templateUrl: './add-device.component.html',
   styleUrls: ['./add-device.component.css']
 })
+
 export class AddDeviceComponent implements OnInit {
 
-  service:DeviceService;
-  constructor(service:DeviceService) { 
+  device: Device;
+  service: DeviceService;
+  constructor(service:DeviceService) {
+    this.device = new Device;
     this.service = service;
   }
 
   ngOnInit(): void {
     
+  }
+
+  handleSubmit(): void{
+    this.service.saveDevice(this.device).subscribe(result => {
+      console.log(result);
+    })
   }
 
 }
