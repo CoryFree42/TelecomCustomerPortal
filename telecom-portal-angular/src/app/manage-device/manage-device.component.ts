4ae7135e2aca2d764d8e01d54a910ccdf6af7c35
Chris4545 <C.ung3232@gmail.com>
2021-08-17 10:32:25 -0500

    Added planManager service to get plans and started work on edit modal for device manager component

--- telecom-portal-angular/src/app/manage-device/manage-device.component.ts
+++ telecom-portal-angular/src/app/manage-device/manage-device.component.ts
@@ -3,6 +3,9 @@ import { DeviceService } from '../device.service';
 import Device from '../models/Device';
 import User from '../models/User';
 import { UserManagerService } from '../user-manager.service';
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+import Plan from '../models/Plan';
+
 
 @Component({
   selector: 'app-manage-device',
@@ -18,11 +21,14 @@ export class ManageDeviceComponent implements OnInit {
   devices:Array<Device>;
   userManager:UserManagerService;
   user:User;
-  constructor(service:DeviceService, userManager:UserManagerService) { 
+  plan:Plan;
+  closeResult = '';
+  constructor(service:DeviceService, userManager:UserManagerService, private modalService: NgbModal) { 
     this.service = service;
     this.devices = [];
     this.userManager = userManager;
     this.user = userManager.getUser();
+    this.plan = new Plan("1", 4, "adwaw", 32.99)
   }
 
   deleteDevice(pNumber:String) {
@@ -34,5 +40,18 @@ export class ManageDeviceComponent implements OnInit {
   ngOnInit(): void {
     this.service.getDevices(this.user.userID).subscribe(result => this.devices = result)
   }
+
+  open(content:any) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+      if(result === "Save click"){
+        console.log(result);
+      }
+
+    }, (reason) => {
+     
+    });
+  }
+
 
 }
