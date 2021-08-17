8941863ba3132769e9b7687146ce10a0994136bd
Chris4545 <C.ung3232@gmail.com>
2021-08-17 11:20:03 -0500

    changed plan type from any to Plan and typecasted results as Plan

--- telecom-portal-angular/src/app/plan-manager.service.ts
+++ telecom-portal-angular/src/app/plan-manager.service.ts
@@ -8,19 +8,21 @@ import Plan from './models/Plan';
 export class PlanManagerService {
 
   url = "http://localhost:9001/plans";
-  plan:any;
-  plan1:any;
-  plan2:any;
+  plan:Plan;
+  plan1:Plan;
+  plan2:Plan;
   constructor(private http:HttpClient) {
     this.plan = new Plan;
     this.plan1 = new Plan;
     this.plan2 = new Plan;
-    this.getPlan();
+    this.getPlans();
+
+
    }
    getPlans(): void{
-    this.http.get(this.url + "/plan/1").subscribe(result => this.plan = result)
-    this.http.get(this.url + "/plan/2").subscribe(result => this.plan1 = result)
-    this.http.get(this.url + "/plan/3").subscribe(result => this.plan2 = result)
+    this.http.get(this.url + "/plan/1").subscribe(result => this.plan = result as Plan)
+    this.http.get(this.url + "/plan/2").subscribe(result => this.plan1 = result as Plan)
+    this.http.get(this.url + "/plan/3").subscribe(result => this.plan2 = result as Plan)
    }
 
    getPlan(): Plan{
