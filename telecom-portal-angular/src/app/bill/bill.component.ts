cd0a0a4e567933664a92f85a69ca1c19f06414a0
Chris4545 <C.ung3232@gmail.com>
2021-08-18 08:42:49 -0500

    Fixed billing page

--- telecom-portal-angular/src/app/bill/bill.component.ts
+++ telecom-portal-angular/src/app/bill/bill.component.ts
@@ -3,6 +3,8 @@ import { BillService } from '../bill.service';
 import { UserService } from '../user.service';
 import User from '../models/User';
 import { UserManagerService } from '../user-manager.service';
+import { PlanManagerService } from '../plan-manager.service';
+import Plan from '../models/Plan';
 
 @Component({
   selector: 'app-bill',
@@ -12,48 +14,42 @@ import { UserManagerService } from '../user-manager.service';
 export class BillComponent implements OnInit {
 
   bill:number;
-  plan1:number;
-  plan2:number;
-  plan3:number;
   user:User;
   billService:BillService;
   userService:UserService;
   umService:UserManagerService;
+  planService:PlanManagerService;
+  plan:Number;
+  plan1:Number;
+  plan2:Number;
 
-  constructor(bservice: BillService, uservice:UserService, userManagerService:UserManagerService) { 
+  constructor(bservice: BillService, uservice:UserService, userManagerService:UserManagerService, planService:PlanManagerService) { 
     this.billService = bservice;
     this.userService = uservice;
     this.bill = 0;
-    this.plan1 = 0;
-    this.plan2 = 0;
-    this.plan3 = 0;
+    this.planService = planService;
     this.umService = userManagerService;
     this.user = userManagerService.getUser();
+    this.plan = 0;
+    this.plan1 = 0;
+    this.plan2 = 0;
 
   }
 
   ngOnInit(): void {
-    this.userService.getAllUsers().subscribe(result => {
-      this.user = result[0];
-      console.log(this.user);
-
       this.billService.getBill(this.user).subscribe(result => {
         this.bill = result;
         console.log(result);
       })
       this.billService.getABill(this.user, 1).subscribe(result => {
-        this.plan1 = result;
+        this.plan = result;
       })
       this.billService.getABill(this.user, 2).subscribe(result => {
-        this.plan2 = result;
+        this.plan1 = result;
       })
       this.billService.getABill(this.user, 3).subscribe(result => {
-        this.plan3 = result;
+        this.plan2 = result;
       })
-   })
-   this.umService.setUser(this.user);
-   console.log(this.umService.getUser());
-   
   }
 
 }
